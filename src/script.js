d358b9a0b013bdd0696ee58366af7ff40ae253a9
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-06-21 13:16:34 -0500

    Added function to show the AI generated recipe

--- src/script.js
+++ src/script.js
@@ -1,18 +1,28 @@
 function generateRecipe(event) {
   event.preventDefault();
 
-  let apiKey = "73fd7aeeb1fc6do18b8423c70f3b718t";
-  let prompt = "Please give me a recipe using these ingredients";
-  let context =
-    "Please use the following ingredients in the recipe: ${ingredients}";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-
   new Typewriter("#recipe", {
-    strings: "This is the recipe",
+    strings: "One moment while we find a recipe for you",
     autoStart: true,
-    delay: 50,
+    delay: 20,
     cursor: null,
   });
+
+  function showRecipe(response) {
+    new Typewriter("#recipe", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 20,
+      cursor: null,
+    });
+  }
+  let givenIngredients = ingredients.value;
+  let apiKey = "73fd7aeeb1fc6do18b8423c70f3b718t";
+  let prompt = `Please give me a recipe using these ingredients: ${givenIngredients}`;
+  let context = "Please respond with no precursor";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showRecipe);
 }
 
 let ingredientElement = document.querySelector("#ingredients-form");
