b1d8e67b9ddd9249ee38f89a67ce2447d418f728
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-06-21 12:49:25 -0500

    Added the API to JS

--- src/script.js
+++ src/script.js
@@ -1,6 +1,12 @@
 function generateRecipe(event) {
   event.preventDefault();
 
+  let apiKey = "73fd7aeeb1fc6do18b8423c70f3b718t";
+  let prompt = "Please give me a recipe using these ingredients";
+  let context =
+    "Please use the following ingredients in the recipe: ${ingredients}";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
   new Typewriter("#recipe", {
     strings: "This is the recipe",
     autoStart: true,
